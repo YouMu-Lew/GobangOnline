f6b2198646e925cf66f041444ea2a21423894e8a
YouMu <lewyoumu@gmail.com>
2024-04-07 22:39:32 +0800

    ADD 新增主菜单和按键UI

---
+++ assets/scripts/MainMenu.ts
@@ -0,0 +1,27 @@
+import { _decorator, Component, director, game, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('MainMenu')
+export class MainMenu extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+
+    }
+
+    on_btn_start() {
+
+    }
+
+    on_btn_setting() { }
+
+    on_btn_about() { }
+
+    on_btn_exit() {
+        game.end();
+    }
+}
+
+
